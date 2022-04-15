20a6a127e05037b805d4c0ca212ef5d78ea93762
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-04-15 15:14:04 +0200

    Create file using Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
 # Create file using Puppet
 
-  file { 'holbertonschool': 
-    ensure => 'present',
-    content => 'I love Puppet', 
-    owner => 'www-data',
-    group => 'www-data',
-    mode => '0744',
-    path => '/tmp/school'
-  }
+file {'/tmp/school':
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
+}
