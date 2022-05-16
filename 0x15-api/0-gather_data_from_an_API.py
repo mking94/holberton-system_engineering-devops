988d7af0b7184430c705937626d044da07c021ae
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-05-16 16:50:59 +0200

    Python script that, using this REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+For a given employee ID, returns information about
+their TODO list progress.
+"""
+
 import requests
 import sys
-"""For a given employee ID, returns information about
-their TODO list progress """
 if __name__ == "__main__":
     userId = sys.argv[1]
     user = requests.get(
