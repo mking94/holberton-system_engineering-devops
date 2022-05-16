acefd7d3aae13e99e7b02c54732a1d0c3de10e26
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-05-16 22:55:50 +0200

    Python script to export data in the JSON format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,12 +9,11 @@ if __name__ == "__main__":
     usr_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
     tds_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
 
-    u = requests.get(usr_url).json()
+    user = requests.get(usr_url).json()
     todo = requests.get(tds_url).json()
 
     with open('{}.csv'.format(id), 'w') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for t in todo:
-            row = [id, u.get("username"), t.get("completed"), t.get("title")]
-            row = [str(value) for value in row]
+        for x in todo:
+            row = [str(id),str(user.get("username")),str(x.get("completed")),str(x.get("completed"))]
             csv_writer.writerow(row)
