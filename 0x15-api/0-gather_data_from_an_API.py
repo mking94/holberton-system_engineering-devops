15da8c2bd5287d9e8f744a1cc22d0dc50e3af833
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-05-16 16:56:27 +0200

    Python script that, using this REST API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,12 +9,12 @@ import sys
 if __name__ == "__main__":
     userId = sys.argv[1]
     user = requests.get(
-        "https://jsonplaceholder.typicode.com/users/{}, verify=False".format(userId))
+        "https://jsonplaceholder.typicode.com/users/{}".format(userId))
     name = user.json().get('name')
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos, verify=False')
-    task = list(filter(lambda x: x['userId'] == 2, todos.json()))
-    comp = list(filter(lambda x: x['completed'], task))
+    req = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = list(filter(lambda x: x['userId'] == 2, req.json()))
+    comp = list(filter(lambda x: x['completed'], todos))
     print('Employee {} is done with tasks({}/{}):'
-          .format(name, len(comp), len(task)))
+          .format(name, len(comp), len(todos)))
     for x in list(x['title'] for x in comp):
         print('\t {}'.format(x))
