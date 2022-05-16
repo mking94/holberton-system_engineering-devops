ee8dac666b53e9416be6917a6f4c4934cd18af55
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-05-16 22:12:43 +0200

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     with open('{}.csv'.format(userId), 'w', encoding='UTF8') as f:
         writer = csv.writer(f)
         for x in task:
-            data = ['{}'.format(x['userId']),'{}'.format(name),'{}'.format(x['completed']),'{}'.format(x['title'])]
-            writer.writerow(data)
+            data = ",".join(['"{0}"'.format(y) for y in data])+"\n"
+            f.write(data)
