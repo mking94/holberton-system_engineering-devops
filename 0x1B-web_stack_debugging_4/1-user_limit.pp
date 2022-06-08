be36504c5a1b4149d5db08c6df58308ecaacde28
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-06-08 14:27:34 +0100

    Create 1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# Enable the user holberton to login.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
